e970745d77d477119dc05ff38f9f14dfb4ae7cb5
Aless6792 <alessandro23500@gmail.com>
2023-11-20 17:01:19 +0100

    First one on Mastermind

---
+++ OneDrive/Documents/Travail/BTS SIO 1ère année/Atelier de professionnalisation/Ap 2.1 MasterMind/MastermindBTS_SIO/Mastermind.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aless
+ * Date: 20/11/2023
+ * Time: 16:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Mastermind
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
