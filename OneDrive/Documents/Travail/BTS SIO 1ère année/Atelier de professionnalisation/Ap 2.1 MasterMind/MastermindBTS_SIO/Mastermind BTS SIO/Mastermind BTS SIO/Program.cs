b60c1347dc5da40af8933503dcf5b834f100e5b9
Aless6792 <alessandro23500@gmail.com>
2023-11-27 17:57:51 +0100

    Apprentissage du RegEX

---
+++ OneDrive/Documents/Travail/BTS SIO 1ère année/Atelier de professionnalisation/Ap 2.1 MasterMind/MastermindBTS_SIO/Mastermind BTS SIO/Mastermind BTS SIO/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aless
+ * Date: 27/11/2023
+ * Time: 16:38
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Mastermind_BTS_SIO
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("1er Joueur : ");
+			Console.Write();
+			char[] combinaison = new char[5];
+			char[] essais = new char[5];
+			char saisie;
+			Console.ReadKey(true);
+		}
+	}
+}
