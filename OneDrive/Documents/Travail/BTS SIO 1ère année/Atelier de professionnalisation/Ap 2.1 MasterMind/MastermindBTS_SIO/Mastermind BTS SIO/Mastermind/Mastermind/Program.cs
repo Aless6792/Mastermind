96403d73e145916496471dde0efdc07c9f499cec
Aless6792 <alessandro23500@gmail.com>
2023-12-04 17:50:59 +0100

    Séance finale

---
+++ OneDrive/Documents/Travail/BTS SIO 1ère année/Atelier de professionnalisation/Ap 2.1 MasterMind/MastermindBTS_SIO/Mastermind BTS SIO/Mastermind/Mastermind/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aless
+ * Date: 04/12/2023
+ * Time: 17:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Mastermind
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
